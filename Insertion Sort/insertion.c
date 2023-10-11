d6f5da33b1aaf09df922ff4530a15aac07c878e7
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2023-10-11 06:14:38 +0200

    Insertion3

--- Insertion Sort/insertion.c
+++ Insertion Sort/insertion.c
@@ -2,7 +2,7 @@ void InsertionSort(int* arr, int dim){
     for(int j=1; j<dim; j++){
         int key = arr[j];
         int i=j-1;
-        while(i>0 && arr[i]>key){
+        while(i>=0 && arr[i]>key){
             arr[i+1]=arr[i];
             i--;
         }
