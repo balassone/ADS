e0beb318b2fd0363d9c5b2822c2e97524f1c2919
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2023-10-23 09:31:55 +0200

    Fixed QuickMinMax

--- QuickMinMax/quickminmax.c
+++ QuickMinMax/quickminmax.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 
 void minmax(int A[], int n, int *min, int *max) {
+
+    int low, high;
+
     if (n % 2 == 0) {
         if (A[0] < A[1]) {
-            *min = A[0];
-            *max = A[1];
+            *(min) = A[0];
+            *(max) = A[1];
         } else {
-            *min = A[1];
-            *max = A[0];
+            *(min) = A[1];
+            *(max) = A[0];
         }
     } else {
-        *min = *max = A[0];
+        *(min) = *(max) = A[0];
     }
 
-    for (int i = 2; i < n; i ++) {
-        int small, big;
-        if (A[i] < A[i + 1]) {
-            small = A[i];
-            big = A[i + 1];
+    for(int i=1; i<=(n+1)/2; i++){
+        if(A[2*i-1-n%2]<A[2*i-n%2]){
+            low=A[2*i-1-n%2];
+            high=A[2*i-n%2];
         } else {
-            small = A[i + 1];
-            big = A[i];
+            low=A[2*i-n%2];
+            high=A[2*i-1-n%2];
         }
 
-        if (small < *min) {
-            *min = small;
+        if(low<*(min)){
+            *(min)=low;
         }
-        if (big > *max) {
-            *max = big;
+        if(high>*(max)){
+            *(max)=high;
         }
     }
+
+    
+    
 }
 
 int main() {
-    int A[] = {1, 2, 3, 4, 5};
+    int A[] = {1, 2, 7,24, 3, 4, 5,19};
+    int n = sizeof(A) / sizeof(int);
     int min, max;
-    minmax(A, 5, &min, &max);
+    minmax(A, n, &min, &max);
     printf("Minimo: %d\n", min);
     printf("Massimo: %d\n", max);
     return 0;
