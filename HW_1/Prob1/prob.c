79bbcae08e9eaea87683c107bcea6da043bbb014
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2023-10-24 16:04:39 +0200

    Finished HW

--- HW_1/Prob1/prob.c
+++ HW_1/Prob1/prob.c
@@ -26,21 +26,49 @@ Divide et impera
 #include <stdlib.h>
 #include <math.h>
 
-int inversionCounter(int* A,int p,int r){
-    if(r-p<=1){
-        return 0;
-    } else if (r-p==2){
-        if(A[p]>A[r-1]){
-            int temp = A[r-1];
-            A[r-1]=A[p];
-            A[p]=temp;
-            return 1;
-        }
-        return 0;
-    } else {
-        
-        return inversionCounter(A,p,r-1) + inversionCounter(A,p+1,r) + inversionCounter(A,p+1,r-1) + inversionCounter(A,0,2);
+int merge(int* A, int p, int q, int r){
+    int n1 = q-p+1;
+    int n2 = r-q;
+    int L[n1], R[n2];
+    int i,j,k,scambi=0;
+    for(i=0; i<n1; i++){
+        L[i]=A[p+i];
+    } for (j=0; j<n2; j++){
+        R[j]=A[q+1+j];
     }
+
+    i=j=0;
+    k=p;
+    while(i<n1 && j<n2){
+        int val;
+        if(L[i]<=R[j]){ //non voglio scambiare elementi uguali
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+            scambi += n1-i; // sto "scavalcando" tutti gli n1-1 elementi che rimangono in L per inserire R[j]
+        }
+        k++;
+    }
+    while(i<n1){
+        A[k++]=L[i++];
+    }
+    while(j<n2){
+        A[k++]=R[j++];
+    }
+    return scambi;
+}
+
+int MergeSort(int* A, int p, int r){
+    int scambi=0;
+    if(p<r){
+        int q = (p+r)/2;
+        scambi+=MergeSort(A,p,q);
+        scambi+=MergeSort(A,q+1,r);
+        scambi+=merge(A,p,q,r);
+    }
+    return scambi;
 }
 
 int main(){
@@ -52,15 +80,9 @@ int main(){
         for(int i=0; i<length; i++){
             fscanf(file,"%d",&A[i]);
         }
-        printf("%d\n",inversionCounter(A,0,length));
-        //for(int i=0; i<length; i++){
-          //  printf("%d\t",A[i]);
-            
-        //}
-        //printf("\n");
+        printf("%d\n",MergeSort(A,0,length-1));
         fscanf(file,"%d",&length);
     } 
     fclose(file);
-    
     return 0;
 }
